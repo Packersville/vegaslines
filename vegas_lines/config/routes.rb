4d3a2e8a41f20af6a13f590d4088edfbf06ba8a1
Tyler Mangan <tyler.mangan@vanderbilt.edu>
2012-01-25 15:35:49 -0600

    Added redirect to user show page upon login. Created user controller.

--- vegas_lines/config/routes.rb
+++ vegas_lines/config/routes.rb
@@ -2,6 +2,8 @@ VegasLines::Application.routes.draw do
   devise_for :users do
     get "/", :to => "devise/sessions#new"
   end
-
+  
+  resources :users, :only => :show
+  
   root :to => "devise/sessions#new"
 end
