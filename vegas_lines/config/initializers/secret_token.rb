278bb601ad5d75877fc5730807a314079976ceba
Tyler Mangan <tyler.mangan@vanderbilt.edu>
2012-01-25 14:54:04 -0600

    Rails project generated

---
+++ vegas_lines/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VegasLines::Application.config.secret_token = '4c6a24dd9c5936636fe6e5ce0862d050e5d5a48b31bd46e3acbb1a06986d3aab7edf21d5034bed1191f56b43c89478b9846d85392aaf4ab0612fb17d5c30aeaf'
