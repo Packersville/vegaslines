7da7cecb1cccbb9e1a688b83cd9c4f67c1ece081
Tyler Mangan <tyler.mangan@vanderbilt.edu>
2012-01-30 14:41:40 -0600

    Week index link to creating games is only displayed for admins.

---
+++ vegas_lines/app/assets/javascripts/weeks.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+  $('#button_show_admin_weeks').click(function() {
+    $('#creating_games_week_table').toggle();
+  });
+});
--- vegas_lines/db/schema.rb
+++ vegas_lines/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120130141637) do
+ActiveRecord::Schema.define(:version => 20120130195531) do
 
   create_table "games", :force => true do |t|
     t.integer  "week_id"
@@ -55,8 +55,8 @@ ActiveRecord::Schema.define(:version => 20120130141637) do
   end
 
   create_table "users", :force => true do |t|
-    t.string   "email",                                 :default => "", :null => false
-    t.string   "encrypted_password",     :limit => 128, :default => "", :null => false
+    t.string   "email",                                 :default => "",    :null => false
+    t.string   "encrypted_password",     :limit => 128, :default => "",    :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -67,6 +67,7 @@ ActiveRecord::Schema.define(:version => 20120130141637) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "admin",                                 :default => false
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
