68e66f1d21db2ee49927bd8d520848c271b3c849
Tyler Mangan <tyler.mangan@vanderbilt.edu>
2012-02-01 15:43:51 -0600

    Removed some code from year controller's index action.

--- vegas_lines/app/controllers/years_controller.rb
+++ vegas_lines/app/controllers/years_controller.rb
@@ -3,12 +3,6 @@ class YearsController < ApplicationController
   
   def index
     @years = Year.all
-    week_ids = @year.weeks.collect {|x| x.id}
-    week_ids = week_ids.map {|i| "'" + i.to_s + "'"}.join(",")
-    lines = Line.where("user_id = #{current_user.id} AND week_id IN (#{week_ids})").count
-    if lines == 0
-      redirect_to user_path(current_user.id)
-    end
   end
   
   def create
