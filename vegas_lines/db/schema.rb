a39836de33aa9dd45b7b908efe419ed36703657f
Tyler Mangan <tyler.mangan@vanderbilt.edu>
2012-02-06 15:09:43 -0600

    Created devise uses and changed the registration views to ask/have first name. Updated Gems.

--- vegas_lines/db/schema.rb
+++ vegas_lines/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120206155615) do
+ActiveRecord::Schema.define(:version => 20120206210659) do
 
   create_table "games", :force => true do |t|
     t.integer  "week_id"
@@ -90,6 +90,7 @@ ActiveRecord::Schema.define(:version => 20120206155615) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "admin",                                 :default => false
+    t.string   "first_name"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
